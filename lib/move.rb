7d5fbd3d20f60aca4d7f23cc26613b8866b9c377
Ciaran Morinan <cmorinan@gmail.com>
2018-06-03 11:29:54 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,9 +7,20 @@ def display_board(board)
 end
 
 def input_to_index(input)
+<<<<<<< HEAD
   input.to_i - 1 
 end
 
 def move(board, index, char="X")
   board[index] = char
+=======
+  input.to_i += 1 
+end
+
+
+
+def move
+  
+  
+>>>>>>> 5f28c35d7f6050e79a641b795dfb46424a1064da
 end
